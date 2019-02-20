5524b479047726f63cc79b0e38a1ca149ae6fac5
awidcha <song.cpe@gmail.com>
2019-02-20 15:49:37 +0700

    normal variable

---
+++ app/src/main/java/com/awidcha/kotlingettingstarted/learning/BasicVariableType.kt
@@ -0,0 +1,17 @@
+package com.awidcha.kotlingettingstarted.learning
+
+/**
+ * Created by song.cpe on 2/20/19.
+ */
+class BasicVariableType {
+    var varInt : Int = 1
+    var varDouble : Double = 3.14
+    var varString : String = "Hello World"
+    var varFloat : Float =  3.14F
+    var varByte : Byte = 123
+    var varShort : Short = 1
+    var varBoolean : Boolean = true
+    var varChar : Char = 'A'
+
+    
+}
